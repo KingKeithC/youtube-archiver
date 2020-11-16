aa976d937ed97f47d4ee0fe1ba31fce82ff150f3
Keith King <KingKeithC@gmail.com>
2020-11-15 17:30:24 -0700

    Misc readability/stability improvements

--- archiver.sh
+++ archiver.sh
@@ -1,27 +1,43 @@
 #! /bin/bash
 
+# Exit immediately on errors
+set -o errexit
+
+# runcmd echoes a command to terminal before evaling it
 function runcmd {
-	echo "Executing command: '$@'..."
+	# Remove tabs from the command before echoing it for better readability
+	echo "Executing command: '$(echo $@ | tr -d \\t)'..."
 	eval "$@"
 }
 
+# errexit prints an error message, then exits the script
 function errexit {
 	echo "$@"
 	exit 1
 }
 
+# Set required variables
 YTAR_DOWNLOAD_BATCH_PATH="${YTAR_DOWNLOAD_BATCH_PATH:-/batchfile}"
 YTAR_DOWNLOAD_ARCHIVE_PATH="${YTAR_DOWNLOAD_ARCHIVE_PATH:-/archivefile}"
 YTAR_DOWNLOAD_TEMP_PATH="${YTAR_DOWNLOAD_TEMP_PATH:-/downloads}"
-YTAR_YTDL_CONFIG_PATH="${YTAR_CONFIG_PATH:-$YTAR_INSTALL_BASE_PATH/ytdl.conf}"
-YTAR_CMD_BASE="python -m youtube_dl --config-location $YTAR_YTDL_CONFIG_PATH"
+
+YTAR_YTDL_CONFIG_PATH="${YTAR_YTDL_CONFIG_PATH:-$YTAR_INSTALL_BASE_PATH/ytdl.conf}"
+YTAR_YTDL_CMD_BASE="python -m youtube_dl --config-location $YTAR_YTDL_CONFIG_PATH"
+
+YTAR_RCLONE_CONFIG_PATH="${YTAR_RCLONE_CONFIG_PATH:-$YTAR_INSTALL_BASE_PATH/rclone.conf}"
+YTAR_RCLONE_CMD_BASE="rclone --config $YTAR_RCLONE_CONFIG_PATH"
 
 mkdir "$YTAR_DOWNLOAD_TEMP_PATH"
 
+# Check that files are in place where needed
 [[ ! -f "$YTAR_DOWNLOAD_BATCH_PATH" ]] 	&& errexit "The path '$YTAR_DOWNLOAD_BATCH_PATH' does not exist!"
 [[ ! -f "$YTAR_DOWNLOAD_ARCHIVE_PATH" ]] && touch $YTAR_DOWNLOAD_ARCHIVE_PATH
 
-# Debug
+# Check that some required variables are set
+[[ -z "$AWS_ACCESS_KEY_ID" ]] 		&& errexit "The variable AWS_ACCESS_KEY_ID is missing or not set."
+[[ -z "$AWS_SECRET_ACCESS_KEY" ]] && errexit "The variable AWS_SECRET_ACCESS_KEY is missing or not set."
+
+# Print variables when debugging
 [[ -n "$YTAR_DEBUG" ]] && set | grep YTAR
 
 echo "Starting..."
