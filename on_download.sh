6d347820c208449b309ce9e2a8d55663e066b4c8
Keith King <KingKeithC@gmail.com>
2020-11-21 22:04:43 -0700

    Correct SRCPATH and DESTPATH variable names

--- on_download.sh
+++ on_download.sh
@@ -9,9 +9,10 @@ if [[ $# -ne 1 ]]; then
 	exit 1
 fi
 
-SRCPATH="$1"
-DESTPATH="main:/$YTAR_S3_BUCKET_NAME/$SRCPATH"
+SRCPATH="$(dirname $1)"
+DESTPATH="main:$YTAR_S3_BUCKET_NAME$SRCPATH"
 
 # Clone the file to to main backend, using the
-echo "Copying $SRCPATH to $DESTPATH ..."
-rclone --config "$YTAR_INSTALL_BASE_PATH/rclone.conf" copy "$FILEPATH" "$DESTPATH"
+echo "Using rclone to copy '$SRCPATH' to '$DESTPATH' ..."
+rclone --config "$YTAR_INSTALL_BASE_PATH/rclone.conf" --verbose --progress copy "$SRCPATH" "$DESTPATH"
+echo "Copy complete"
