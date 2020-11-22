4a7cbd6f5534cfa197d5b95425c6ebdf679c66e3
Keith King <KingKeithC@gmail.com>
2020-11-22 15:55:21 -0700

    Simplify youtube-dl output format

--- archiver.sh
+++ archiver.sh
@@ -34,6 +34,6 @@ echo "Starting Download from YouTube..."
 python -m youtube_dl \
  --config-location "$YTAR_YTDL_CONFIG_PATH" \
  --download-archive "$YTAR_ARCHIVE_PATH/archivefile" \
- --output "$YTAR_DOWNLOADS_PATH/channels/%(channel_id)s/playlists/%(playlist_id)s/%(playlist_index)s_%(release_date)s_%(id)s_%(title)s.%(ext)s" \
+ --output "$YTAR_DOWNLOADS_PATH/%(channel)s/playlists/%(playlist_title)s/%(playlist_index)s_%(title)s.%(ext)s" \
  --exec "$YTAR_INSTALL_BASE_PATH/on_download.sh {}" \
  $*
