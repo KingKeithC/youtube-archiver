945a6bebca2b75ac42872b07ace1fe218183b74f
Keith King <KingKeithC@gmail.com>
2020-11-21 21:25:04 -0700

    Rename rclone command from clone to copy

--- on_download.sh
+++ on_download.sh
@@ -11,4 +11,4 @@ DESTPATH="main:/$YTAR_S3_BUCKET_NAME/$SRCPATH"
 
 # Clone the file to to main backend, using the
 echo "Copying $SRCPATH to $DESTPATH ..."
-rclone --config "$YTAR_INSTALL_BASE_PATH/rclone.conf" clone "$FILEPATH" "main:/$YTAR_S3_BUCKET_NAME/$FILEPATH"
+rclone --config "$YTAR_INSTALL_BASE_PATH/rclone.conf" copy "$FILEPATH" "main:/$YTAR_S3_BUCKET_NAME/$FILEPATH"
